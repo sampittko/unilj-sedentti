0b089b83fb07f23f8f75174798dd4ad775daa9ff
Branislav Puzder <branislav.puzder@student.tuke.sk>
2019-10-26 18:06:38 +0200

    basic project structure

--- app/src/main/java/sk/tuke/ms/sedentti/ui/statistics/StatisticsFragment.java
+++ app/src/main/java/sk/tuke/ms/sedentti/ui/statistics/StatisticsFragment.java
@@ -1,4 +1,4 @@
-package sk.tuke.ms.sedentti.ui.dashboard;
+package sk.tuke.ms.sedentti.ui.statistics;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,17 +13,17 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import sk.tuke.ms.sedentti.R;
 
-public class DashboardFragment extends Fragment {
+public class StatisticsFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
+    private StatisticsViewModel statisticsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        statisticsViewModel =
+                ViewModelProviders.of(this).get(StatisticsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_statistics, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(this, new Observer<String>() {
+        statisticsViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/sk/tuke/ms/sedentti/ui/statistics/StatisticsViewModel.java
+++ app/src/main/java/sk/tuke/ms/sedentti/ui/statistics/StatisticsViewModel.java
@@ -1,16 +1,16 @@
-package sk.tuke.ms.sedentti.ui.notifications;
+package sk.tuke.ms.sedentti.ui.statistics;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class StatisticsViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public StatisticsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is statistics fragment");
     }
 
     public LiveData<String> getText() {
