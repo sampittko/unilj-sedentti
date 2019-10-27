0a8d2121c1833425842f8da03cc41f65dcb53972
Branislav Puzder <branislav.puzder@student.tuke.sk>
2019-10-27 21:37:59 +0100

    profile fragment ui and session history activity

---
+++ app/src/main/java/sk/tuke/ms/sedentti/SessionHistoryActivity.java
@@ -0,0 +1,14 @@
+package sk.tuke.ms.sedentti;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SessionHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_session_history);
+    }
+}
--- app/src/main/java/sk/tuke/ms/sedentti/ui/profile/ProfileFragment.java
+++ app/src/main/java/sk/tuke/ms/sedentti/ui/profile/ProfileFragment.java
@@ -1,14 +1,20 @@
 package sk.tuke.ms.sedentti.ui.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProviders;
+import sk.tuke.ms.sedentti.MotivateMeActivity;
 import sk.tuke.ms.sedentti.R;
 
 public class ProfileFragment extends Fragment {
@@ -20,5 +26,29 @@ public class ProfileFragment extends Fragment {
         profileViewModel = ViewModelProviders.of(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
         return root;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.profile_activity_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_motivate_me:
+                startActivity(new Intent(getContext(), MotivateMeActivity.class));
+                return true;
+            case R.id.action_settings:
+                return false;
+        }
+        return false;
     }
 }
