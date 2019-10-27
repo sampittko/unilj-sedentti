119bc8ec0f3db6009e368cee67cebedf65944626
Branislav Puzder <branislav.puzder@student.tuke.sk>
2019-10-27 17:01:49 +0100

    fix move ui from statistics to home fragment

--- app/src/main/java/sk/tuke/ms/sedentti/ui/statistics/StatisticsFragment.java
+++ app/src/main/java/sk/tuke/ms/sedentti/ui/statistics/StatisticsFragment.java
@@ -1,17 +1,9 @@
 package sk.tuke.ms.sedentti.ui.statistics;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.hookedonplay.decoviewlib.DecoView;
-import com.hookedonplay.decoviewlib.charts.SeriesItem;
-import com.hookedonplay.decoviewlib.events.DecoEvent;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -27,51 +19,6 @@ public class StatisticsFragment extends Fragment {
         statisticsViewModel =
                 ViewModelProviders.of(this).get(StatisticsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_statistics, container, false);
-//        final TextView textView = root.findViewById(R.id.text_dashboard);
-//        statisticsViewModel.getText().observe(this, new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
         return root;
-    }
-
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        List<DecoView> graphs = new ArrayList<>();
-        graphs.add((DecoView) getView().findViewById(R.id.graph_f_home_active));
-        graphs.add((DecoView) getView().findViewById(R.id.graph_f_home_sedentary));
-        graphs.add((DecoView) getView().findViewById(R.id.graph_f_home_session));
-
-        for (DecoView graph : graphs) {
-            // Create background track
-            graph.addSeries(new SeriesItem.Builder(Color.argb(255, 218, 218, 218))
-                    .setRange(0, 100, 100)
-                    .setInitialVisibility(false)
-                    .setLineWidth(32f)
-                    .build());
-
-//Create data series track
-            SeriesItem seriesItem1 = new SeriesItem.Builder(Color.argb(255, 64, 196, 0))
-                    .setRange(0, 100, 0)
-                    .setLineWidth(32f)
-                    .build();
-
-            int series1Index = graph.addSeries(seriesItem1);
-
-            graph.addEvent(new DecoEvent.Builder(DecoEvent.EventType.EVENT_SHOW, true)
-                    .setDelay(1000)
-                    .setDuration(2000)
-                    .build());
-
-            graph.addEvent(new DecoEvent.Builder(25).setIndex(series1Index).setDelay(4000).build());
-            graph.addEvent(new DecoEvent.Builder(100).setIndex(series1Index).setDelay(8000).build());
-            graph.addEvent(new DecoEvent.Builder(10).setIndex(series1Index).setDelay(12000).build());
-        }
-
-
     }
 }
