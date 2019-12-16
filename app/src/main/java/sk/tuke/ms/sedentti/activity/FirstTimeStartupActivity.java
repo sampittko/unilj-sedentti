5eeb4704ae510ee657b37a4ed481fe6929777154
Samuel Pitoňák <sampittko@gmail.com>
2019-12-16 15:09:00 +0100

    - add missing permissions - request permissions from the user - make app permissions-dependent

--- app/src/main/java/sk/tuke/ms/sedentti/activity/FirstTimeStartupActivity.java
+++ app/src/main/java/sk/tuke/ms/sedentti/activity/FirstTimeStartupActivity.java
@@ -1,18 +1,23 @@
 package sk.tuke.ms.sedentti.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.crashlytics.android.Crashlytics;
 import com.facebook.stetho.Stetho;
-import com.j256.ormlite.android.apptools.OpenHelperManager;
+
+import org.jetbrains.annotations.NotNull;
 
 import sk.tuke.ms.sedentti.R;
+import sk.tuke.ms.sedentti.config.PredefinedValues;
 import sk.tuke.ms.sedentti.helper.shared_preferences.AppSPHelper;
-import sk.tuke.ms.sedentti.model.config.DatabaseHelper;
 
 public class FirstTimeStartupActivity extends AppCompatActivity {
 
@@ -28,10 +33,45 @@ public class FirstTimeStartupActivity extends AppCompatActivity {
 
         Stetho.initializeWithDefaults(this);
 
+        if (permissionsGranted()) {
+            Crashlytics.log(Log.DEBUG, TAG, "Permissions already granted");
+            decideNextStep();
+        }
+        else {
+            Crashlytics.log(Log.DEBUG, TAG, "Requesting missing permissions");
+            requestMissingPermissions();
+        }
+    }
+
+    private boolean permissionsGranted() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestMissingPermissions() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                PredefinedValues.PERMISSION_REQUEST_CODE_READ_WRITE_EXTERNAL_STORAGE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NotNull String[] permissions, @NotNull int[] grantResults) {
+        if (requestCode == PredefinedValues.PERMISSION_REQUEST_CODE_READ_WRITE_EXTERNAL_STORAGE) {
+            if (grantResults.length > 0 &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                Crashlytics.log(Log.DEBUG, TAG, "Permissions were granted successfully");
+                decideNextStep();
+            } else {
+                Crashlytics.log(Log.ERROR, TAG, "Permissions were not granted by the user, quitting app as a consequence");
+                finish();
+            }
+        }
+    }
+
+    private void decideNextStep() {
         AppSPHelper appSPHelper = new AppSPHelper(this);
         appSPHelper.setAppDefaultSettings();
-
-        // TODO request all required permissions
 
         if (appSPHelper.firstTimeStartupPerformed()) {
             Crashlytics.log(Log.DEBUG, TAG, "First time startup had already been performed before");
